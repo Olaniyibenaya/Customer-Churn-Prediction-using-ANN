08495fcec3deb0e8ab7aba52638daff7508a2dca
Folaranmi Olaniyi Erastus <141184131+Olaniyibenaya@users.noreply.github.com>
2025-11-07 13:40:42 +0100

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -57,13 +57,6 @@ geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_featur
 # Merge everything
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
-# ✅ Ensure same column order as during training
-expected_columns = scaler.feature_names_in_  # works if you trained scaler in sklearn >=1.0
-missing_cols = [col for col in expected_columns if col not in input_data.columns]
-
-# Add any missing columns with 0s
-for col in missing_cols:
-    input_data[col] = 0
 
 # Reorder columns to match the scaler
 input_data = input_data[expected_columns]
@@ -81,3 +74,4 @@ if prediction_proba > 0.5:
 else:
 
     st.info(f'The customer is not likely to churn (Probability: {prediction_proba:.2f})')
+
